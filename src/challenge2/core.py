abc4e3a26bb25e303526f515713e11946bc6c728
scaracciolo <santiago.caracciolo@gmail.com>
2024-04-09 14:07:42 -0300

    remove filtering of categories in curation and pipeline functions

--- src/challenge2/core.py
+++ src/challenge2/core.py
@@ -1,13 +1,11 @@
 from pathlib import Path 
 import pandas as pd
-from string import ascii_uppercase
-
-# all sorted from low to high price.
-CUT_CATEGORIES = ('Fair', 'Good', 'Very Good', 'Premium', 'Ideal')
-CLARITY_CATEGORIES = ('I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF')
-COLOR_CATEGORIES = list(reversed(ascii_uppercase[3:10]))
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder 
+from sklearn.linear_model import LinearRegression 
 
 def load(file_path: Path) -> pd.DataFrame:
+    # here, some check before load file
     return pd.read_csv(file_path)
 
 def curation(input: pd.DataFrame) -> None:
@@ -28,18 +26,31 @@ def curation(input: pd.DataFrame) -> None:
     rec = input.query('(price <= 0) or (x <= 0) or (y <=0) or (z <= 0) or (depth <= 0) or (table <= 0)')
     input.drop(index=rec.index, inplace=True)
 
-    rec = input[~input.cut.isin(CUT_CATEGORIES)]
-    input.drop(index=rec.index, inplace=True)
+def encoding(input: pd.DataFrame) -> pd.DataFrame:
 
-    input[~input.color.isin(COLOR_CATEGORIES)]
-    input.drop(index=rec.index, inplace=True)
-    
-    input[~input.clarity.isin(CLARITY_CATEGORIES)]
-    input.drop(index=rec.index, inplace=True)
+    enc = OneHotEncoder(sparse_output=False)
+    cat_data = input.get(['cut', 'clarity', 'color']).to_numpy()
+    enc.fit(cat_data) # if new classes appear will be added
 
-def encoding(input: pd.DataFrame) -> None:
+    ohe_data = pd.DataFrame(
+        data=enc.transform(cat_data),
+        columns=np.hstack(enc.categories_),
+        index=input.index,
+        dtype='bool'
+    )
 
-    input.cut = input.cut.map({k:v+1 for v, k in enumerate(CUT_CATEGORIES)})
-    input.color = input.color.map({k:v+1 for v, k in enumerate(COLOR_CATEGORIES)})
-    input.clarity = input.clarity.map({k:v+1 for v, k in enumerate(CLARITY_CATEGORIES)})
+    return ohe_data
+
+def build_model(file_path: Path) -> LinearRegression:
+    input = load(file_path)
+    curation(input)
+    ohe_data = encoding(input)
+    X = input.drop(columns=['price', 'cut','color','clarity']).join(ohe_data)
+    X -= X.mean() # standarizing samples
+    X /= X.std()
+    log_y = input.price.map(np.log).to_numpy() # target
+    return LinearRegression().fit(X, log_y)
+
+def predict(model: LinearRegression, y: np.ndarray) -> np.ndarray:
+    return np.exp(model.predict(np.log(y)))
 
