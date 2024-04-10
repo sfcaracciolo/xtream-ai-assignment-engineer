a830496b50bf4fc972189d6afb289442f93666d4
scaracciolo <santiago.caracciolo@gmail.com>
2024-04-10 15:23:06 -0300

    challenge2: enhancing help and separating load/build

--- src/challenge2/builder.py
+++ src/challenge2/builder.py
@@ -1,25 +1,27 @@
 from argparse import ArgumentParser
 from pathlib import Path 
-from src.challenge2.core import build_model 
+from src.challenge2.core import build_model, load_csv
 
 parser = ArgumentParser(prog='Model builder')
-parser.add_argument('-s', 
-                    help='Path of the csv file',
+parser.add_argument('-s', metavar='csv path', 
+                    help='Absolute or relative path of the csv file with diamonds data.',
                     required=True,
+                    dest='CSV_PATH',
                     type=Path
                     )
-parser.add_argument('-d',
-                    help='Destination path of the model including filename.',
+parser.add_argument('-d', metavar='model path',
+                    help='Absolute or relative destination path of the model without extension.',
                     required=True,
+                    dest='MODEL_PATH',
                     type=Path
                     )
-
 
 if __name__ == '__main__':
     args = parser.parse_args()
     try:
-        build_model(args.s, model_path=args.d)
-    except:
-        print('error while building model.')
+        data = load_csv(args.CSV_PATH)
+        _ = build_model(data, model_path=args.MODEL_PATH.with_suffix('.pickle'))
+    except Exception as exc:
+        print(exc)
     else:
         print('model saved.')
