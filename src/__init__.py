f33c811cea9e4a230cd637252702e82e74a1ab52
scaracciolo <santiago.caracciolo@gmail.com>
2024-04-11 16:07:07 -0300

    load env vars

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,13 @@
-from . import challenge1
 from . import challenge2
+
+from dotenv import load_dotenv
+from pathlib import Path 
+import os 
+
+load_dotenv()
+
+DB_FILE = Path(os.getenv('DB_FILE'))
+CSV_FILE = Path(os.getenv('CSV_FILE'))
+CURRENT_MODEL_PATH = Path(os.getenv('CURRENT_MODEL_PATH'))
+BACKUP_MODELS_PATH = Path(os.getenv('BACKUP_MODELS_PATH'))
+ROOT_PATH = Path(__file__).parent.parent
