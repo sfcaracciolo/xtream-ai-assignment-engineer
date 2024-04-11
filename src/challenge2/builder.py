f236635881bf71455a6d7904c21555d7e453a8c4
scaracciolo <santiago.caracciolo@gmail.com>
2024-04-11 16:05:10 -0300

    challenge2: refactor to class

--- src/challenge2/builder.py
+++ src/challenge2/builder.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 from pathlib import Path 
-from src.challenge2.core import build_model, load_csv
+from src.challenge2.core import Model
+import pandas as pd
 
 parser = ArgumentParser(prog='Model builder')
 parser.add_argument('-s', metavar='csv path', 
@@ -10,7 +11,7 @@ parser.add_argument('-s', metavar='csv path',
                     type=Path
                     )
 parser.add_argument('-d', metavar='model path',
-                    help='Absolute or relative destination path of the model without extension.',
+                    help='Absolute or relative destination filepath of the model.',
                     required=True,
                     dest='MODEL_PATH',
                     type=Path
@@ -19,8 +20,9 @@ parser.add_argument('-d', metavar='model path',
 if __name__ == '__main__':
     args = parser.parse_args()
     try:
-        data = load_csv(args.CSV_PATH)
-        _ = build_model(data, model_path=args.MODEL_PATH.with_suffix('.pickle'))
+        data = pd.read_csv(args.CSV_PATH)
+        model = Model.build(data)
+        model.save(args.MODEL_PATH.parent, filename=args.MODEL_PATH.stem)
     except Exception as exc:
         print(exc)
     else:
--- src/challenge2/core.py
+++ src/challenge2/core.py
@@ -1,67 +1,103 @@
-from pathlib import Path 
+from pathlib import Path
+from typing import Self 
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import OneHotEncoder 
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression 
 import pickle 
+from string import ascii_uppercase
 
-def load_csv(file_path: Path) -> pd.DataFrame:
-    # here, some checks before load file
-    return pd.read_csv(file_path)
+class Model(LinearRegression):
 
-def curation(input: pd.DataFrame) -> None:
+    cut_cat = np.array(('Fair', 'Good', 'Very Good', 'Premium', 'Ideal'), dtype=object)
+    color_cat = np.array(list(reversed(ascii_uppercase[3:10])), dtype=object) # J to D
+    clarity_cat = np.array(('I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF'), dtype=object)
+    features = ['carat', 'cut', 'color', 'clarity', 'depth', 'table', 'x', 'y', 'z']
     
-    # removing nans
-    rec = input[input.isna().any(axis=1)]
-    input.drop(index=rec.index, inplace=True)
+    def __init__(self, *, fit_intercept: bool = True, copy_X: bool = True, n_jobs = None, positive: bool = False) -> None:
+        super().__init__(fit_intercept=fit_intercept, copy_X=copy_X, n_jobs=n_jobs, positive=positive)
+        self.scaler = StandardScaler()
 
-    # removing nulls
-    rec = input[input.isnull().any(axis=1)]
-    input.drop(index=rec.index, inplace=True)
+    @staticmethod
+    def curation_inplace(ds: pd.DataFrame) -> None:
+        # removing nans
+        rec = ds[ds.isna().any(axis=1)]
+        ds.drop(index=rec.index, inplace=True)
 
-    # removing duplicated rows
-    rec = input[input.duplicated()]
-    input.drop(index=rec.index, inplace=True)
+        # removing nulls
+        rec = ds[ds.isnull().any(axis=1)]
+        ds.drop(index=rec.index, inplace=True)
 
-    # removing out of range records
-    rec = input.query('(price <= 0) or (x <= 0) or (y <=0) or (z <= 0) or (depth <= 0) or (table <= 0)')
-    input.drop(index=rec.index, inplace=True)
+        # removing duplicated rows
+        rec = ds[ds.duplicated()]
+        ds.drop(index=rec.index, inplace=True)
 
-def encoding(input: pd.DataFrame) -> pd.DataFrame:
+        # removing out of range records
+        rec = ds.query('(price <= 0) or (x <= 0) or (y <=0) or (z <= 0) or (depth <= 0) or (table <= 0)')
+        ds.drop(index=rec.index, inplace=True)
 
-    enc = OneHotEncoder(sparse_output=False)
-    cat_data = input.get(['cut', 'clarity', 'color']).to_numpy()
-    enc.fit(cat_data) # if new classes appear will be added
+        # removing invalid classes
+        rec = ds[~ds.cut.isin(Model.cut_cat)]
+        ds.drop(index=rec.index, inplace=True)
 
-    ohe_data = pd.DataFrame(
-        data=enc.transform(cat_data),
-        columns=np.hstack(enc.categories_),
-        index=input.index,
-        dtype='bool'
-    )
+        rec = ds[~ds.color.isin(Model.color_cat)]
+        ds.drop(index=rec.index, inplace=True)
 
-    return ohe_data
+        rec = ds[~ds.clarity.isin(Model.clarity_cat)]
+        ds.drop(index=rec.index, inplace=True)
 
-def build_model(input: pd.DataFrame, model_path: Path = None) -> None | LinearRegression:
-    curation(input)
-    ohe_data = encoding(input)
-    X = input.drop(columns=['price', 'cut','color','clarity']).join(ohe_data)
-    X -= X.mean() # standarizing samples
-    X /= X.std()
-    log_y = input.price.map(np.log).to_numpy() # target
-    model = LinearRegression().fit(X, log_y)
+    @staticmethod
+    def featuring_inplace(ds: pd.DataFrame) -> None: 
+        width = .5*(ds.x+ds.y) # estimation of width.
+        ds.loc[:,'depth'] = 100 * ds.z / width
 
-    if model_path is not None:
-        save_model(model, model_path)
+    @staticmethod
+    def encoding_inplace(ds: pd.DataFrame):
+        """ds header: cut, color and clarity"""
+        ds.loc[:,'cut'] = ds.cut.map({k:i+1 for i,k in enumerate(Model.cut_cat)})
+        ds.loc[:,'color'] = ds.color.map({k:i+1 for i,k in enumerate(Model.color_cat)})
+        ds.loc[:,'clarity'] = ds.clarity.map({k:i+1 for i,k in enumerate(Model.clarity_cat)})
+
+    @staticmethod
+    def extract_features(ds: pd.DataFrame) -> pd.DataFrame:
+        return ds.get(Model.features)
     
-    return model
+    @staticmethod
+    def extract_target(ds: pd.DataFrame) -> pd.Series:
+        return ds.get(['price'])
+    
+    def fit(self, X: pd.DataFrame, y: pd.Series) -> LinearRegression:
+        self.featuring_inplace(X)
+        self.encoding_inplace(X)
+        scaled_X = self.scaler.fit_transform(X)
+        scaled_y = np.log(y)
+        return super().fit(scaled_X, scaled_y)
+    
+    def predict(self, X: pd.DataFrame) -> np.ndarray:
+        self.featuring_inplace(X)
+        self.encoding_inplace(X)
+        scaled_X = self.scaler.transform(X)
+        y = super().predict(scaled_X)
+        return np.exp(y)
+    
+    def save(self, path: Path, filename: str = 'model'):
+        path.mkdir(parents=True, exist_ok=True)
+        pickle.dump(self, open(path / f'{filename}.pickle', 'wb'))
 
-def save_model(model: LinearRegression, model_path: Path) -> None:
-    pickle.dump(model, open(model_path, 'wb'))
+    @staticmethod
+    def load(path: Path) -> LinearRegression:
+        if path.is_dir():
+            path = path / 'model.pickle'
+        return pickle.load(open(path, 'rb'))
+    
+    @classmethod
+    def build(cls, ds: pd.DataFrame) -> Self:
+        model = cls()
+        model.curation_inplace(ds)
+        X = model.extract_features(ds)
+        y = model.extract_target(ds)
+        model.fit(X, y)
+        return model
 
-def load_model(model_path: Path) -> LinearRegression:
-    return pickle.load(open(model_path, 'rb'))
 
-def predict(model: LinearRegression, samples: np.ndarray) -> np.ndarray:
-    return np.exp(model.predict(samples))
 
